2c89e56824b3619463b0f9c6252f87edda98d22b
Dmitrii Posadskii <dmitry.posadsky@gmail.com>
2018-12-15 13:04:46 +0300

    [151218] Kata. Tribonacci

---
+++ kata/src/test/java/dev/posadskiy/kata/XbonacciTest.java
@@ -0,0 +1,37 @@
+package dev.posadskiy.kata;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+public class XbonacciTest {
+    private Xbonacci variabonacci;
+
+    @Before
+    public void setUp() throws Exception {
+        variabonacci = new Xbonacci();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        variabonacci = null;
+    }
+
+    private double precision = 1e-10;
+
+    @Test
+    public void basicTests() {
+        assertArrayEquals(new double []{1,1,1,3,5,9,17,31,57,105}, variabonacci.tribonacci(new double []{1,1,1},10), precision);
+        assertArrayEquals(new double []{0,0,1,1,2,4,7,13,24,44}, variabonacci.tribonacci(new double []{0,0,1},10), precision);
+        assertArrayEquals(new double []{0,1,1,2,4,7,13,24,44,81}, variabonacci.tribonacci(new double []{0,1,1},10), precision);
+    }
+
+    @Test
+    public void basicTestsBetter() {
+        assertArrayEquals(new double []{1,1,1,3,5,9,17,31,57,105}, variabonacci.better(new double []{1,1,1},10), precision);
+        assertArrayEquals(new double []{0,0,1,1,2,4,7,13,24,44}, variabonacci.better(new double []{0,0,1},10), precision);
+        assertArrayEquals(new double []{0,1,1,2,4,7,13,24,44,81}, variabonacci.better(new double []{0,1,1},10), precision);
+    }
+}
+
