221a950101943219835e7b54d11268fdb6b3feaf
Dmitrii Posadskii <dmitry.posadsky@gmail.com>
2018-12-18 21:36:27 +0300

    [181218] Util. Create module for util methods, add sort() method to new class.

---
+++ util/src/test/java/dev/posadskiy/util/CollectionsTest.java
@@ -0,0 +1,11 @@
+package dev.posadskiy.util;
+
+import org.junit.Test;
+
+public class CollectionsTest {
+
+    @Test
+    public void sort_NullCollection_ReturnNull() {
+        Collections.sort(null);
+    }
+}
