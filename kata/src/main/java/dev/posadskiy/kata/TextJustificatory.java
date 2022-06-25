0a17f846ae80786b174840ef6db0708a123f2bd1
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2022-06-25 23:34:15 +0200

    [250622] Kata. Text align justify

---
+++ kata/src/main/java/dev/posadskiy/kata/TextJustificatory.java
@@ -0,0 +1,74 @@
+package dev.posadskiy.kata;
+
+/**
+ * Your task in this Kata is to emulate text justification in monospace font. You will be given a single-lined text and the expected justification width. The longest word will never be greater than this width.
+ *
+ * Here are the rules:
+ *
+ * Use spaces to fill in the gaps between words.
+ * Each line should contain as many words as possible.
+ * Use '\n' to separate lines.
+ * Gap between words can't differ by more than one space.
+ * Lines should end with a word not a space.
+ * '\n' is not included in the length of a line.
+ * Large gaps go first, then smaller ones ('Lorem--ipsum--dolor--sit-amet,' (2, 2, 2, 1 spaces)).
+ * Last line should not be justified, use only one space between words.
+ * Last line should not contain '\n'
+ * Strings with one word do not need gaps ('somelongword\n').
+ * 
+ * Link: https://www.codewars.com/kata/537e18b6147aa838f600001b/java
+ */
+public class TextJustificatory {
+    public static String justify(String text, int width) {
+        final String[] source = text.split(" ");
+
+        StringBuilder result = new StringBuilder();
+        StringBuilder string = new StringBuilder();
+        for (String word : source) {
+            if (word.length() + string.length() + 1 > width) {
+                result.append(
+                    addJustified(string, width)
+                )
+                    .append("\n");
+                string
+                    .delete(0, string.length());
+            }
+
+            if (string.length() != 0) string.append(" ");
+            
+            string.append(word);
+        }
+        
+        result.append(
+            string
+        );
+        
+        return result.toString();
+    }
+    
+    private static String addJustified(StringBuilder raw, int width) {
+        final String[] words = raw.toString().split(" ");
+        if (words.length == 0) return "";
+        if (words.length == 1) return words[0];
+
+        final int freeSpaces = width - raw.length();
+        final int additionalSpacesPerWord = freeSpaces / (words.length - 1);
+        final int additionalSpacesForFirstWords = freeSpaces % (words.length - 1);
+
+        StringBuilder result = new StringBuilder(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            result.append(" ");
+            if (i <= additionalSpacesForFirstWords) result.append(" ");
+
+            for (int i1 = 0; i1 < additionalSpacesPerWord; i1++) {
+                result.append(" ");
+            }
+            
+            if (i != words.length - 1) result.append(words[i]);
+        }
+        
+        result.append(words[words.length - 1]);
+
+        return result.toString();
+    }
+}
---
+++ kata/src/test/java/dev/posadskiy/kata/TextJustificatoryTest.java
@@ -0,0 +1,42 @@
+package dev.posadskiy.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TextJustificatoryTest {
+    
+    @Test
+    public void sampleTests() {
+        assertEquals("123  45\n6", TextJustificatory.justify("123 45 6", 7));
+        assertEquals("123", TextJustificatory.justify("123", 7));
+        assertEquals("", TextJustificatory.justify("", 10));
+    }
+
+    private static final String LIPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sa"
+        + "gittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusc"
+        + "e at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. V"
+        + "ivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, no"
+        + "n dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at f"
+        + "ermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum vel"
+        + "it ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellent"
+        + "esque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla"
+        + " et dolor.";
+
+    @Test
+    public void descriptionExample() {
+        String expected = "Lorem  ipsum  dolor  sit amet,\nconsectetur  adipiscing  elit.\n"
+            + "Vestibulum    sagittis   dolor\nmauris,  at  elementum  ligula\ntempor  eget.  In quis rhoncus\n"
+            + "nunc,  at  aliquet orci. Fusce\nat   dolor   sit   amet  felis\nsuscipit   tristique.   Nam  a\n"
+            + "imperdiet   tellus.  Nulla  eu\nvestibulum    urna.    Vivamus\ntincidunt  suscipit  enim, nec\n"
+            + "ultrices   nisi  volutpat  ac.\nMaecenas   sit   amet  lacinia\narcu,  non dictum justo. Donec\n"
+            + "sed  quam  vel  risus faucibus\neuismod.  Suspendisse  rhoncus\nrhoncus  felis  at  fermentum.\n"
+            + "Donec lorem magna, ultricies a\nnunc    sit    amet,   blandit\nfringilla  nunc. In vestibulum\n"
+            + "velit    ac    felis   rhoncus\npellentesque. Mauris at tellus\nenim.  Aliquam eleifend tempus\n"
+            + "dapibus. Pellentesque commodo,\nnisi    sit   amet   hendrerit\nfringilla,   ante  odio  porta\n"
+            + "lacus,   ut   elementum  justo\nnulla et dolor.";
+
+        assertEquals(expected, TextJustificatory.justify(LIPSUM, 30));
+    }
+
+}
