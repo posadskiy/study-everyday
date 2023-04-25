330369cbe7b92c8af53ac03af3b3d9c9e8285c23
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-04-25 18:46:37 +0200

    [250423] Kata. Rgb to Hex

---
+++ kata/src/main/java/dev/posadskiy/kata/RgbToHex.java
@@ -0,0 +1,14 @@
+package dev.posadskiy.kata;
+
+public class RgbToHex {
+    public static String rgb(int r, int g, int b) {
+        return String.format("%02X%02X%02X", validate(r), validate(g), validate(b));
+    }
+    
+    private static int validate(int value) {
+        if (value > 255) return 255;
+        if (value < 0) return 0;
+        
+        return value;
+    }
+}
