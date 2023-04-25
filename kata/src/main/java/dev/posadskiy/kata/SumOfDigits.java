6d1ab1f577bd51316bf9e6211499b3d1e3d5aea4
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-04-25 18:02:59 +0200

    [250423] Kata. Sum of digits

---
+++ kata/src/main/java/dev/posadskiy/kata/SumOfDigits.java
@@ -0,0 +1,16 @@
+package dev.posadskiy.kata;
+
+import java.util.Arrays;
+
+public class SumOfDigits {
+    public static int digital_root(int n) {
+        int value = n;
+        while (value > 9) {
+            value = Integer.parseInt(
+                Arrays.stream(String.valueOf(value).split(""))
+                    .reduce("0", (v1, v2) -> String.valueOf(Integer.parseInt(v1) + Integer.parseInt(v2))));
+        }
+        
+        return value;
+    }
+}
