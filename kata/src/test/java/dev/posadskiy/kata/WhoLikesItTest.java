427febe453e126034a6ce4a8eb7ab810fdf1eba8
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-08-22 17:29:12 +0200

    [220823] Kata. Who likes it?

---
+++ kata/src/test/java/dev/posadskiy/kata/WhoLikesItTest.java
@@ -0,0 +1,50 @@
+package dev.posadskiy.kata;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class WhoLikesItTest {
+    private static String[] names = new String[]{"Brian", "Marilyn", "Pamela", "Timothy", "Jesse", "Heather", "Terry", "Carlos",
+        "Bonnie", "Randy", "Lillian", "Emily", "Louis", "Chris", "Howard", "Helen", "Ralph", "Jennifer", "Mark", "Laura",
+        "Jason", "Shirley", "Diane", "Phillip", "David", "Joan", "Wanda", "Jimmy", "Carl", "Betty", "Adam", "Lawrence",
+        "Kathleen", "Mildred", "Rose", "Tina", "Jose", "Keith", "Janice", "Maria", "Kenneth", "Arthur", "James", "Catherine",
+        "Henry", "Denise", "Ruby", "Cynthia", "Anthony", "Jeffrey", "Eugene", "Dorothy", "Lori", "Bobby", "Peter", "Alice",
+        "Eric", "Wayne", "Phyllis", "Roger", "Clarence", "Scott", "John", "Philip", "Teresa", "Andrea", "Douglas", "Earl",
+        "Melissa", "Benjamin", "Rebecca", "Michelle", "Alan", "Brenda", "William", "Frank", "Matthew", "George", "Anna",
+        "Cheryl", "Roy", "Paula", "Tammy", "Gerald", "Christina", "Russell", "Kelly", "Albert", "Donna", "Todd", "Jessica",
+        "Kimberly", "Johnny", "Dennis", "Jack", "Doris", "Martha", "Stephanie", "Stephen", "Christine"};
+
+    @Test
+    public void staticTests() {
+        assertEquals("no one likes this", WhoLikesIt.whoLikesIt());
+        assertEquals("Peter likes this", WhoLikesIt.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", WhoLikesIt.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", WhoLikesIt.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", WhoLikesIt.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+        assertEquals("Brian, Marilyn and 98 others like this", WhoLikesIt.whoLikesIt(names));
+    }
+
+    @Test
+    public void randomTests() {
+        Random rand = new Random();
+        for (int i = 0; i < 24; i++) {
+            int r = rand.nextInt(100);
+            String[] randNames = new String[r];
+            for (int j = 0; j < r; j++) randNames[j] = names[rand.nextInt(100)];
+            assertEquals(whoLikesIt(randNames), WhoLikesIt.whoLikesIt(randNames));
+        }
+    }
+
+    private String whoLikesIt(String... names) {
+        switch (names.length) {
+            case 0:  return "no one likes this";                //feels bad man
+            case 1:  return names[0] + " likes this";
+            case 2:  return String.format("%s and %s like this", names[0], names[1]);
+            case 3:  return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        }
+    }
+}
