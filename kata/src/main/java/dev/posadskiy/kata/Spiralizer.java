226d03c313ac5538266b9e4ba9dc3ec98cf9be57
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-08-23 19:25:36 +0200

    [230823] Kata. Spiralizer

---
+++ kata/src/main/java/dev/posadskiy/kata/Spiralizer.java
@@ -0,0 +1,41 @@
+package dev.posadskiy.kata;
+
+public class Spiralizer {
+    public static int[][] spiralize(int size) {
+        int[][] array = new int[size][size];
+
+        int lastX = array.length;
+        int lastY = array.length;
+        int firstX = 0;
+        int firstY = 0;
+
+        while (lastX > firstX && lastY > firstY) {
+            final int start = firstY == 0 ? firstY : firstY - 1;
+            for (int i = start; i < lastY; ++i) {
+                array[firstX][i] = 1;
+            }
+            if (lastY - start < 2) return array;
+            firstX += 2;
+
+            for (int i = firstX - 1; i < lastX; ++i) {
+                array[i][lastY - 1] = 1;
+            }
+            if (lastX - firstY - 1 < 2) return array;
+            lastY -= 2;
+
+            for (int i = lastY + 1; i > firstY; --i) {
+                array[lastX - 1][i - 1] = 1;
+            }
+            if (lastY + 1 - firstY < 2) return array;
+            lastX -= 2;
+
+            for (int i = lastX + 1; i > firstX; --i) {
+                array[i - 1][firstY] = 1;
+            }
+            if (lastX + 1 - firstX < 2) return array;
+            firstY += 2;
+        }
+
+        return array;
+    }
+}
---
+++ kata/src/test/java/dev/posadskiy/kata/SpiralizerTest.java
@@ -0,0 +1,51 @@
+package dev.posadskiy.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpiralizerTest {
+    @Test
+    public void test5() {
+        assertArrayEquals(
+            new int[][] {
+                { 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 0, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1 }
+            },
+            Spiralizer.spiralize(5));
+    }
+    
+    @Test
+    public void test6() {
+        assertArrayEquals(
+            new int[][] {
+                { 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 0, 1 },
+                { 1, 0, 0, 1, 0, 1 },
+                { 1, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1 }
+            },
+            Spiralizer.spiralize(6));
+    }
+
+    @Test
+    public void test8() {
+        assertArrayEquals(
+            new int[][] {
+                { 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 0, 1 },
+                { 1, 0, 0, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1 },
+            },
+            Spiralizer.spiralize(8));
+    }
+
+}
