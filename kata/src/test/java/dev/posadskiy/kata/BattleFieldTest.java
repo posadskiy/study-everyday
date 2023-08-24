5dbea423810a5a6bf82f34f30737a81302bff420
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-08-24 22:45:33 +0200

    [240823] Kata. Battle Field Validator

---
+++ kata/src/test/java/dev/posadskiy/kata/BattleFieldTest.java
@@ -0,0 +1,24 @@
+package dev.posadskiy.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BattleFieldTest {
+    private static int[][] battleField = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+        {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+        {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+    @Test
+    public void SampleTest() {
+        assertEquals(true, BattleField.fieldValidator(battleField));
+    }
+
+}
