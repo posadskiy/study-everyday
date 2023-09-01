19c1af8c88dc592e89200ae039bb54dcf263e80d
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-09-01 23:28:12 +0200

    [010923] Language. Java. Releases. v9. Unified JVM Logging

---
+++ language/java/releases/version_9/src/UnifiedJvmLogging.java
@@ -0,0 +1,16 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * JEP 158: Unified JVM Logging
+ * <a href="https://openjdk.org/jeps/158">Link</a>
+ */
+public class UnifiedJvmLogging {
+
+    public static void main(String[] args) {
+        Logger.getGlobal().log(Level.INFO, "Some output");
+        Logger.getGlobal().log(Level.WARNING, "Some another output");
+        Logger.getGlobal().log(Level.SEVERE, "More output");
+    }
+
+}
