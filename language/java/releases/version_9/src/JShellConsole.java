8ea39f2a7557c3e5a698dbaeb20d275b98cc7a84
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-09-02 17:48:05 +0200

    [020923] Language. Java. Releases. v9. JShell Console

---
+++ language/java/releases/version_9/src/JShellConsole.java
@@ -0,0 +1,8 @@
+/**
+ * EP 222: jshell: The Java Shell (Read-Eval-Print Loop)
+ * Tools -> JShell Console...
+ * <a href="https://openjdk.org/jeps/222">Doc</a>
+ */
+public class JShellConsole {
+
+}
