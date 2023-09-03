e0515321ee40db8bd03851ae40abe80230496f02
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-09-03 09:32:03 +0200

    [020923] Language. Java. Releases. v9. More concurrency updates

---
+++ language/java/releases/version_9/src/features/concurrency/MoreConcurrencyUpdates.java
@@ -0,0 +1,40 @@
+package features.concurrency;
+
+import java.lang.System.Logger.Level;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * JEP 266: More Concurrency Updates
+ * 
+ * <a href="http://www.reactive-streams.org">Reactive streams docs</a>
+ * <a href="https://github.com/reactive-streams/reactive-streams-jvm/tree/v1.0.4/examples/src">Reactive streams example</a>
+ */
+public class MoreConcurrencyUpdates {
+
+    final static ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    public static void main(String[] args) {
+
+        final CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "Hola!")
+            .thenApplyAsync(value -> {
+                System.getLogger("default").log(Level.INFO, Thread.currentThread().getName());
+
+                return value;
+            }, executorService);
+
+        try {
+            System.getLogger("default").log(Level.INFO, future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.getLogger("default").log(Level.INFO, future.isDone());
+
+        executorService.shutdown();
+    }
+
+}
+
---
+++ language/java/releases/version_9/src/features/concurrency/ProductionRunner.java
@@ -0,0 +1,15 @@
+package features.concurrency;
+
+public class ProductionRunner {
+
+    public static void main(String[] args) {
+        final Producer producer = new Producer();
+        final Consumer consumerOne = new Consumer();
+        final Consumer consumerTwo = new Consumer();
+        producer.subscribe(consumerOne);
+        producer.subscribe(consumerTwo);
+        producer.createProduct();
+        producer.createProduct();
+    }
+
+}
