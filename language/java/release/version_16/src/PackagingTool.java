856c828b44bd810413c3911ea8a185c33cc8b474
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-09-09 21:28:55 +0200

    [030923] Language. Java. Release. v16. Packaging Tool

---
+++ language/java/release/version_16/src/PackagingTool.java
@@ -0,0 +1,13 @@
+
+/**
+ * JEP 392: Packaging Tool
+ * <p>
+ * Usage example: jpackage --input target/ \ --name JPackageDemoApp \ --main-jar version_16-0.1-SNAPSHOT.jar \ --main-class Unix \ --type
+ * dmg \ --java-options '--enable-preview'
+ * <p>
+ * <a href="https://openjdk.org/jeps/392">Docs</a>
+ * <a href="https://www.baeldung.com/java14-jpackage">Example</a>
+ */
+public class PackagingTool {
+
+}
