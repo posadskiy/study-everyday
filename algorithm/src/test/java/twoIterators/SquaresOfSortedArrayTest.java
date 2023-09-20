d440d54d6ee38b3b5e43ea027832152e997eee49
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-09-20 22:19:31 +0200

    [200923] Algorithm. Two iterators. SquaresOfSortedArray

---
+++ algorithm/src/test/java/twoIterators/SquaresOfSortedArrayTest.java
@@ -0,0 +1,17 @@
+package twoIterators;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SquaresOfSortedArrayTest {
+
+    @Test
+    public void testOne() {
+        SquaresOfSortedArray squaresOfSortedArray = new SquaresOfSortedArray();
+
+        final int[] result = squaresOfSortedArray.sortedSquares(new int[]{-4, -1, 0, 3, 10});
+        
+        assertArrayEquals(new int[]{0,1,9,16,100}, result);
+    }
+}
