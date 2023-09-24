269f58ef7c255eb176e47e53ed4e1af69a2b2c6a
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-09-24 11:50:22 +0200

    [240923] Algorithm. Breadth-First Search.

---
+++ algorithm/src/main/java/breadthFirstSearch/TreeNodeWithAccess.java
@@ -0,0 +1,30 @@
+package breadthFirstSearch;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNodeWithAccess<T> {
+
+    @Getter
+    private T value;
+
+    @Getter
+    private List<TreeNodeWithAccess<T>> children;
+
+    public TreeNodeWithAccess(T value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+    
+    public TreeNodeWithAccess<T> of(T value) {
+        return new TreeNodeWithAccess<>(value);
+    }
+
+    public TreeNodeWithAccess<T> add(T value) {
+        final TreeNodeWithAccess<T> child = new TreeNodeWithAccess<>(value);
+        children.add(child);
+        return child;
+    }
+}
