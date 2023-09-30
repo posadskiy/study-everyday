9669a8c94dcb6509b9cdc8ee63364b2302f79ec2
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-09-30 22:59:26 +0200

    [300923] Kata. Best Time To Buy And Sell Stock

---
+++ kata/src/test/java/dev/posadskiy/kata/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,14 @@
+package dev.posadskiy.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BestTimeToBuyAndSellStockTest {
+    
+    @Test
+    public void test() {
+        assertEquals(new BestTimeToBuyAndSellStock().maxProfit(new int[]{7,1,5,3,6,4}), 5);
+    }
+
+}
