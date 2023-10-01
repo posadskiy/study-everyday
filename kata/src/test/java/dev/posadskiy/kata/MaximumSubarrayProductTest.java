4c8bf85ebede8e5220582d06fe889b10f5350389
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-10-01 22:27:37 +0200

    [011023] Kata. Maximum Subarray Product

---
+++ kata/src/test/java/dev/posadskiy/kata/MaximumSubarrayProductTest.java
@@ -0,0 +1,14 @@
+package dev.posadskiy.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaximumSubarrayProductTest {
+    
+    @Test
+    public void test() {
+        assertEquals(MaximumSubarrayProduct.sequenceBest(new int[]{2,-5,-2,-4,3}), 24);
+    }
+
+}
