d8f6db5ed4b9faf0bfc34fd7217702576c42a3c1
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2023-11-20 17:00:37 +0100

    [201123] Languages. Java. Releases. v18. Code Snippet

---
+++ language/java/release/version_18/src/main/java/com/posadskiy/java/release/v18/CodeSnippet.java
@@ -0,0 +1,17 @@
+package com.posadskiy.java.release.v18;
+
+/**
+ * JEP 413: Code Snippets in Java API Documentation
+ * <a href="https://openjdk.org/jeps/413">Docs</a>
+ * <p>
+ * Code snipped is below
+ * {@snippet :
+ * if (value == null) {
+ *     System.out.println("Provided value is null"); // @highlight substring="println"
+ * }
+ *}
+ * <p>
+ * Here is additional information about class
+ */
+public class CodeSnippet {
+}
