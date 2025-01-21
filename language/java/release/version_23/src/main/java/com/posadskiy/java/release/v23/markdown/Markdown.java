5d6f1956a0d3e1e623be0ec798d728836ea9500e
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2025-01-21 07:41:57 +0100

    [210125] Language. Java. Release. v23. Add Markdown support example for JEP 467

---
+++ language/java/release/version_23/src/main/java/com/posadskiy/java/release/v23/markdown/Markdown.java
@@ -0,0 +1,10 @@
+package com.posadskiy.java.release.v23.markdown;
+
+
+/// ### JEP 467: Markdown Documentation Comments
+///
+/// Enable JavaDoc documentation comments to be written in Markdown rather than solely in a mixture of HTML and JavaDoc @-tags.
+///
+/// [Link](https://openjdk.org/jeps/467)
+public class Markdown {
+}
